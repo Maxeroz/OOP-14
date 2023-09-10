a1bcbae9dea23f3c4220c4f38a2ef63b02fa6ae4
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-09-10 22:32:50 +0400

    Prototypal Inheritance on Built-In Objects

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 ///////////////////////////////////////
 // What is Object-Oriented Programming?
 
@@ -61,4 +61,33 @@ console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
 
 ///////////////////////////////////////
-// Prototypal Inheritance and The Prototype Chai
+// Prototypal Inheritance and The Prototype Chain
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+*/
+
+const newF = _ => 10;
+console.dir(newF.__proto__);
