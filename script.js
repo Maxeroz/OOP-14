f1191c76adf3480b2c96672ab950a41e5182405d
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-09-12 17:12:58 +0400

    ES6 Classes

--- script.js
+++ script.js
@@ -87,7 +87,6 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
-*/
 
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -99,13 +98,19 @@ const Car = function (make, speed) {
 
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes', 95);
+
 Car.prototype.accelerate = function () {
   this.speed += 10;
-  console.log(this.speed);
+  this.printSpeed();
 };
+
 Car.prototype.brake = function () {
   this.speed -= 5;
-  console.log(this.speed);
+  this.printSpeed();
+};
+
+Car.prototype.printSpeed = function () {
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 bmw.accelerate();
@@ -113,3 +118,39 @@ bmw.brake();
 
 mercedes.accelerate();
 mercedes.brake();
+*/
+
+///////////////////////////////////////
+// ES6 Classes
+
+// class expression
+// const PersonCl = class{}
+
+// class declaration
+class PersonCl {
+  constructor(fristName, birthYear) {
+    this.fristName = fristName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fristName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.fristName}`);
+// };
+
+jessica.greet();
