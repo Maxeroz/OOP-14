2414fea7b79f2b03e95f47e54ff0524d4506db3a
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-09-15 21:51:30 +0400

    Coding Challenge #3

--- script.js
+++ script.js
@@ -279,7 +279,6 @@ const ford = new CarCl('Ford', 120);
 
 ford.speedUS = 50;
 console.log(ford);
-*/
 
 ///////////////////////////////////////
 // Inheritance Between "Classes": Constructor Functions
@@ -317,3 +316,56 @@ console.log(mike instanceof Object);
 Student.prototype.constructor = Student;
 console.log(mike);
 console.dir(Student.prototype.constructor);
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  this.printSpeed();
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  this.printSpeed();
+};
+
+Car.prototype.printSpeed = function () {
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+tesla.chargeBattery(50);
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
